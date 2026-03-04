6f9acc7faa028a5071460750cb366dd311da7939
gabogara <gabo1gara@gmail.com>
2026-03-03 23:27:47 -0800

    feat: add initial data array for cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const flashcards = [
+  {
+    id: 1,
+    front: "Apple",
+    back: "Manzana",
+    category: "Food",
+    difficulty: "easy",
+  },
+  {
+    id: 2,
+    front: "Bread",
+    back: "Pan",
+    category: "Food",
+    difficulty: "easy",
+  },
+  {
+    id: 3,
+    front: "Strawberry",
+    back: "Fresa",
+    category: "Food",
+    difficulty: "medium",
+  },
+  {
+    id: 4,
+    front: "I’m hungry.",
+    back: "Tengo hambre.",
+    category: "Food",
+    difficulty: "easy",
+  },
+  {
+    id: 5,
+    front: "Could I have the menu, please?",
+    back: "¿Me puede traer el menú, por favor?",
+    category: "Food",
+    difficulty: "medium",
+  },
 
-function App() {
-  const [count, setCount] = useState(0)
+  {
+    id: 6,
+    front: "Airport",
+    back: "Aeropuerto",
+    category: "Travel",
+    difficulty: "easy",
+  },
+  {
+    id: 7,
+    front: "Ticket",
+    back: "Boleto / billete",
+    category: "Travel",
+    difficulty: "easy",
+  },
+  {
+    id: 8,
+    front: "Where is the bathroom?",
+    back: "¿Dónde está el baño?",
+    category: "Travel",
+    difficulty: "easy",
+  },
+  {
+    id: 9,
+    front: "I’m lost.",
+    back: "Estoy perdido(a).",
+    category: "Travel",
+    difficulty: "easy",
+  },
+  {
+    id: 10,
+    front: "Could you show me on the map?",
+    back: "¿Podrías mostrármelo en el mapa?",
+    category: "Travel",
+    difficulty: "medium",
+  },
+];
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+  return <div>App</div>;
+};
 
-export default App
+export default App;
